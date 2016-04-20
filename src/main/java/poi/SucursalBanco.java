8cfd725ff58e3f56101aaabf81245a739b1d292b
mujicajuancarlos <mujica.juancarlos@gmail.com>
2016-04-20 02:57:07 -0300

    Agrego metodo estaDisponible para la clase Sucursal Banco

--- src/main/java/poi/SucursalBanco.java
+++ src/main/java/poi/SucursalBanco.java
@@ -1,14 +1,62 @@
 package poi;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import usuario.Posicion;
 
 public class SucursalBanco extends POI{
 	
+	private ArrayList<DayOfWeek> diasDeAtencion;
+	private LocalTime horaCierre;
+	private LocalTime horaApertura;
+	
 	public SucursalBanco(List<String> etiquetas,Posicion posicion) {
 		this.etiquetas = etiquetas;
 		this.posicion = posicion;
+		this.agregarDiaDeAtencion(DayOfWeek.MONDAY);
+		this.agregarDiaDeAtencion(DayOfWeek.TUESDAY);
+		this.agregarDiaDeAtencion(DayOfWeek.WEDNESDAY);
+		this.agregarDiaDeAtencion(DayOfWeek.THURSDAY);
+		this.agregarDiaDeAtencion(DayOfWeek.FRIDAY);
+		this.setHoraApertura(LocalTime.of(10,0,0));
+		this.setHoraCierre(LocalTime.of(15,0,0));
 	}
-
+	private void setHoraCierre(LocalTime hora) {
+		this.horaCierre = hora;		
+	}
+	private void setHoraApertura(LocalTime hora) {
+		this.horaApertura = hora;
+	}
+	private void agregarDiaDeAtencion(DayOfWeek unDia) {
+		this.diasDeAtencion.add(unDia);
+	}
+	public boolean estaDisponible(LocalDateTime unaFechaHora) {
+		// verifico tanto el dia como la hora
+		
+		return this.esUnDiaDisponible(unaFechaHora) && this.esUnaHoraDisponible(unaFechaHora);
+	}
+	private boolean esUnaHoraDisponible(LocalDateTime unaFechaHora) {
+		LocalTime horaActual = LocalTime.of(
+				unaFechaHora.getHour(),
+				unaFechaHora.getMinute(),
+				unaFechaHora.getSecond()
+				);
+		return ((this.getHoraApertura().isBefore(horaActual) ) && (this.getHoraCierre().isAfter(horaActual)));
+	}
+	private LocalTime getHoraCierre() {
+		return this.horaCierre;
+	}
+	private LocalTime getHoraApertura() {
+		return this.horaApertura;
+	}
+	private boolean esUnDiaDisponible(LocalDateTime unaFechaHora) {
+		return this.getDiasDeAtencion().contains(unaFechaHora.getDayOfWeek());
+	}
+	private ArrayList<DayOfWeek> getDiasDeAtencion() {
+		return this.diasDeAtencion;
+	}
 }
