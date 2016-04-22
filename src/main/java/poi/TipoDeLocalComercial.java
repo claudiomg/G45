47dec8fde424edd2e3435c1ffc06d16dc9d3973d
mujicajuancarlos <mujica.juancarlos@gmail.com>
2016-04-22 00:33:00 -0300

    agrego la clase abstracta de tipo de local comercial

---
+++ src/main/java/poi/TipoDeLocalComercial.java
@@ -0,0 +1,17 @@
+package poi;
+
+public abstract class TipoDeLocalComercial implements Cloneable {
+	
+	protected double RADIO_CERCANIA;
+
+	public TipoDeLocalComercial clone() throws CloneNotSupportedException {
+		  return (TipoDeLocalComercial) super.clone();
+	}
+	
+	public double getRadioDeCercania(){
+		return RADIO_CERCANIA;
+	}
+	protected void setRadioDeCercania(double radio) {
+		RADIO_CERCANIA = radio;
+	}
+}
