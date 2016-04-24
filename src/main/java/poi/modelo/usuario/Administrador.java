80424347fe18755f5bd2a2de4e54299daa2c7a03
cecilialuna <cecilialuna.94@hotmail.com>
2016-04-23 22:16:50 -0300

    agrego clase administrador con sus metodos de remover, modificar y agregar pois

---
+++ src/main/java/poi/modelo/usuario/Administrador.java
@@ -0,0 +1,29 @@
+package poi.modelo.usuario;
+
+import poi.utilidades.RepositorioPOI;
+import poi.utilidades.Posicion;
+import poi.modelo.puntoDeInteres.POI;
+
+public class Administrador {
+	
+	private RepositorioPOI repositorio;
+	
+	public Administrador(RepositorioPOI repositorio){
+		this.repositorio = repositorio;
+	}
+		public void agregarPOI(POI poi){
+			this.repositorio.agregarPOI(poi);			
+	}	
+		public void removerPOI (POI poi){
+			this.repositorio.removerPOI(poi);
+	}
+		public void modificarPOI(POI poi, Posicion posicion, String etiqueta, String etiqueta2){
+			this.repositorio.modificarPOI(poi, posicion, etiqueta, etiqueta2);
+	}
+		
+			
+}
+		
+
+
+
