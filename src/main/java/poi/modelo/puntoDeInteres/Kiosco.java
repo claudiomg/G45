d84e7e8a37c0d036dc029830389f394450eaba24
mujicajuancarlos <mujica.juancarlos@gmail.com>
2016-05-29 21:33:54 -0300

    Elimino las clases KIosco y LibreriaEscolar

--- src/main/java/poi/modelo/puntoDeInteres/Kiosco.java
+++
@@ -1,8 +0,0 @@
-package poi.modelo.puntoDeInteres;
-
-public class Kiosco extends LocalComercial{	
-	
-	public Kiosco() {
-		this.RADIO_CERCANIA= 0.2 ;
-	}
-}
--- src/main/java/poi/modelo/puntoDeInteres/LibreriaEscolar.java
+++
@@ -1,8 +0,0 @@
-package poi.modelo.puntoDeInteres;
-
-public class LibreriaEscolar extends LocalComercial{	
-	
-	public LibreriaEscolar() {
-		this.RADIO_CERCANIA =0.5;
-	}
-}
