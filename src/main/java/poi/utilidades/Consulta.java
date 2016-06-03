6aca463ab216585eae9956dd59ee2f56a0243445
cecilialuna <cecilialuna.94@hotmail.com>
2016-06-03 12:55:01 -0300

    vuelvo para atras la consulta porque rompe toda la busqueda por palabra

--- src/main/java/poi/utilidades/Consulta.java
+++ src/main/java/poi/utilidades/Consulta.java
@@ -1,30 +1,25 @@
 package poi.utilidades;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import poi.modelo.puntoDeInteres.POI;
 import poi.repositorios.RepositorioPOI;
-import poi.repositorios.RepositorioCGPExternos;
+
 
 public class Consulta {
 	
 	private RepositorioPOI repositorio;
 	private List<POI> poisEncontrados = new ArrayList<POI>();
-	private RepositorioCGPExternos repositorioCGPExterno;
+	
 
 	
 	public Consulta(RepositorioPOI repositorio) {
 		this.repositorio = repositorio;
 	}
 	
-	public void setRepositorioCGPExterno(RepositorioCGPExternos repositorioCGPExterno){
-		this.repositorioCGPExterno= repositorioCGPExterno;
-	}
-
 	public List<POI> getPoisEncontrados() {
 		return poisEncontrados;
 	}
@@ -44,10 +39,6 @@ public class Consulta {
 		return repositorio.getPois().stream().filter(unPOI -> unPOI.getEtiquetas().contains(palabra));
 	}
 	
-	public Stream<POI> filtrarPorPalabra2 (String palabra){
-		return repositorioCGPExterno.getPois().stream().filter(unPOI -> unPOI.getEtiquetas().contains(palabra));
-		
-	}
 	
 	public List<POI> asignarAPoisEncontrados(Stream<POI> lista){
 		return this.poisEncontrados = lista.collect(Collectors.toCollection(ArrayList::new));
