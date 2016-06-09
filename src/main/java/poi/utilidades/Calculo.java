d72f05c1cafa1cc9a2b3e175cff6f731fc2ae158
Nicolas Perez <nico__1993@hotmail.com>
2016-06-09 16:34:34 -0300

    calculo en kiliometros usando api google

--- src/main/java/poi/utilidades/Calculo.java
+++ src/main/java/poi/utilidades/Calculo.java
@@ -1,19 +1,40 @@
 package poi.utilidades;
 
 import java.util.List;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.internal.StringJoin.UrlValue;
+import com.google.maps.model.LatLng;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.GeocodingApiRequest;
+
 
 public class Calculo {
 
-
+	
 	public static double distanciaEnKilometros(Posicion unaPosicion, Posicion otraPosicion){
-		double theta = unaPosicion.longitud - otraPosicion.longitud;
-		double distancia = Math.sin((unaPosicion.latitud * Math.PI)/180.0) * Math.sin((otraPosicion.latitud * Math.PI)/180.0) 
-				+ Math.cos((unaPosicion.latitud * Math.PI)/180.0) * Math.cos((otraPosicion.latitud * Math.PI)/180.0) * Math.cos((theta * Math.PI)/180.0);
-		distancia = Math.acos(distancia);
-		distancia = distancia * 180.0/ Math.PI;
-		distancia = distancia * 60 * 1.1515;
-		distancia = distancia * 1.609344;
-		return (distancia);
+		double lat = unaPosicion.longitud;
+		double lon =unaPosicion.longitud ;
+		double lat2 = otraPosicion.longitud;
+		double lon2 =otraPosicion.longitud ;
+		GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyDV2WMczZfNd2j01NNBByj75mBRz7AFeac");
+		
+		
+		LatLng inicio = new LatLng(lat, lon);
+		LatLng fin = new LatLng(lat2, lon2);
+		
+				String[] origen  = reverseGeocode(context,inicio);
+				String[] destino = reverseGeocode(context,fin);
+		
+
+		
+					
+		DistanceMatrixApiRequest request =	newRequest(context);
+		request = getDistanceMatrix( context,origen ,destino );
 		
 }
 	public static double distanciaLineal(Posicion unaPosicion, Posicion otraPosicion){
