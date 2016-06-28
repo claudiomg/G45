5be7c89575378c6cf3242a6563793ad566ab8162
mujicajuancarlos <mujica.juancarlos@gmail.com>
2016-06-28 11:43:39 -0300

    Agrego los metodos abstractos createResult y dumpReport para que puedan ser implementados en cada uno de los reportes del sistema

--- src/main/java/poi/reportes/ReporteAbstracto.java
+++ src/main/java/poi/reportes/ReporteAbstracto.java
@@ -1,5 +1,15 @@
 package poi.reportes;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 public abstract class ReporteAbstracto {
 
+	public abstract JsonArray dumpReport();
+	//este metodo debe ser llamado desde el controller para armar la grilla de resultados
+	
+	protected abstract JsonObject createResult();
+	//por cada resultado se debe generar un JsonObject y se debe agregar al JsonArray de resultados
+
 }
+
--- src/main/java/poi/reportes/ReporteBusquedasPorTerminal.java
+++ src/main/java/poi/reportes/ReporteBusquedasPorTerminal.java
@@ -1,5 +1,20 @@
 package poi.reportes;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 public class ReporteBusquedasPorTerminal extends ReporteAbstracto {
+
+	@Override
+	public JsonArray dumpReport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected JsonObject createResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
