b77f995267b106490ba2fd08d25e25a14730d9ff
cecilialuna <cecilialuna.94@hotmail.com>
2016-06-30 00:12:35 -0300

    no se que estoy haciendo :S :S

---
+++ src/main/java/poi/controllers/AdminController.java
@@ -0,0 +1,19 @@
+package poi.controllers;
+
+
+
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class AdminController {
+
+	
+	public ModelAndView administrar(Request request, Response response){
+		return null;
+	
+	}
+	
+}
