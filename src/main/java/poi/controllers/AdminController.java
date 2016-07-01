afe82858b6f76561124a8b4d4e56532742294840
cecilialuna <cecilialuna.94@hotmail.com>
2016-06-30 21:00:56 -0300

    administrador controller cambios

--- src/main/java/poi/controllers/AdminController.java
+++ src/main/java/poi/controllers/AdminController.java
@@ -12,6 +12,19 @@ public class AdminController {
 
 	
 	public ModelAndView administrar(Request request, Response response){
+		
+		HashMap<String, Object> viewModel = new HashMap<>();
+		
+		if (request.params("btnAdmPOI") != null){
+			return new ModelAndView(viewModel, "abmPois.html");
+		} else if (request.params("btnReporte") != null){
+			return new ModelAndView(viewModel, "reportesBusquedas.html");
+		} else if (request.params("btnAdmTerm") != null){
+			return new ModelAndView(viewModel, "adminTerminales.html");
+		}
+		
+		
+		
 		return null;
 	
 	}
