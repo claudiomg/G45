eb46ae408ec760ccc79ee1f25aaa3285193b869d
mujicajuancarlos <mujica.juancarlos@gmail.com>
2016-07-03 20:21:00 -0300

    agrego un mediador para que resuelva nuestas peticiones

--- src/main/java/poi/finders/RequestMediator.java
+++ src/main/java/poi/finders/RequestMediator.java
@@ -1,5 +1,10 @@
 package poi.finders;
 
+import java.time.LocalDateTime;
+
+import poi.modelo.usuario.RepositorioTerminal;
+import poi.modelo.usuario.Terminal;
+import poi.utilidades.Posicion;
 import spark.Request;
 
 public class RequestMediator {
@@ -9,6 +14,44 @@ public class RequestMediator {
 	public RequestMediator(Request request) {
 		this.setConcreteRequest(request);
 	}
+	
+	public String palabraBuscada() {
+		return this.getConcreteRequest().queryParams("palabra");
+	}
+	
+	public boolean enabledDisponibilityFilter() {
+		return this.getConcreteRequest().queryParams("disponibilityFilter").equals("ON");
+	}
+	
+	public boolean enabledProximityFilter() {
+		return this.getConcreteRequest().queryParams("proximityFilter").equals("ON");
+	}
+	
+	public boolean enabledSearchOfCgpService() {
+		return this.getConcreteRequest().queryParams("cgpServiceFilter").equals("ON");
+	}
+
+	public boolean enabledSearchOfBank() {
+		return this.getConcreteRequest().queryParams("bankFilter").equals("ON");
+	}
+	
+	public LocalDateTime getDateAndTimeForDisponibility() {
+		//De ser necesario devolver la fecha y hora seleccionada en el cliente, por ahora solo devuelve la del sistema
+		return LocalDateTime.now();
+	}
+	
+	public Posicion getPositionForProximity() {
+		Double longitud = new Double(this.getConcreteRequest().queryParams("longitud"));
+		Double latitud = new Double(this.getConcreteRequest().queryParams("latitud"));
+		return new Posicion(latitud, longitud);
+	}
+	
+	public Terminal user() {
+		// devuelvo la instancia de usuario alojada en la session
+		// cuando se agregue la session modificar este metodo
+		//return this.getConcreteRequest().session().attribute("user");
+		return RepositorioTerminal.getInstance().terminales.get(0);
+	}
 
 	private void setConcreteRequest(Request request) {
 		this.concreteRequest = request;
@@ -17,5 +60,4 @@ public class RequestMediator {
 	private Request getConcreteRequest() {
 		return this.concreteRequest;
 	}
-
 }
