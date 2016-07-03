57367126e710452c2f7289cd7ec7779433d041c2
mujicajuancarlos <mujica.juancarlos@gmail.com>
2016-07-03 20:21:44 -0300

    Agrego los metodos necesarios para la creacion de un finder

--- src/main/java/poi/finders/PoiFinder.java
+++ src/main/java/poi/finders/PoiFinder.java
@@ -1,10 +1,35 @@
 package poi.finders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import poi.repositorios.RepositorioAbstracto;
+import poi.utilidades.Consulta;
+
 public class PoiFinder {
+
+	private Consulta consulta;
+	private List<PoiFilter> filters = new ArrayList<PoiFilter>();
+	private List<RepositorioAbstracto> repositories = new ArrayList<RepositorioAbstracto>();
 
 	public void search() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public void setConsulta(Consulta query) {
+		this.consulta = query;
+	}
+	public Consulta getConsulta() {
+		return this.consulta;
+	}
+
+	public void addFilter(PoiFilter unFiltro) {
+		this.filters.add(unFiltro);
+	}
+
+	public void addRepository(RepositorioAbstracto unRepositorio) {
+		this.repositories.add(unRepositorio);
 	}
 
 }
