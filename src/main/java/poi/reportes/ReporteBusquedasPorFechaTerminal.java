6fa140b3e7018f13ef3ee9bc498ccd19312c840c
mujicajuancarlos <mujica.juancarlos@gmail.com>
2016-07-05 17:27:59 -0300

    Agrego test para los reportes de terminales y fechas

--- src/main/java/poi/reportes/ReporteBusquedasPorFechaTerminal.java
+++ src/main/java/poi/reportes/ReporteBusquedasPorFechaTerminal.java
@@ -1,37 +1,51 @@
 package poi.reportes;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.HashMap;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import poi.modelo.usuario.Terminal;
-import poi.repositorios.RepositorioConsultas;
+import poi.utilidades.Consulta;
 
 public class ReporteBusquedasPorFechaTerminal extends ReporteAbstracto {
 	
-	
-
 	@Override
 	public void dumpReport() {
-		//poner aqui el comportamiento
+		HashMap<Terminal, HashMap<LocalDate, Integer>> terminales = this.recolectarTerminales();
+		this.loadResults(terminales);
 	}
 	
-	public JsonObject consultasHechasEnTerminalEnciertaFecha(String unaTerminal,LocalDate unaFecha){
-		Long unaCantidad;
-		unaCantidad = this.getRepository().cantidaDeConsultasHechasEnUnaTerminalEnUnaFecha(unaTerminal,unaFecha);
-		return createResult(unaTerminal,unaFecha,unaCantidad);
+	private void loadResults(HashMap<Terminal, HashMap<LocalDate, Integer>> terminales) {
+		for (Terminal terminal : terminales.keySet()){
+			for (LocalDate fecha : terminales.get(terminal).keySet()){
+				this.createResult(
+					terminal.getUsuario(),
+					fecha,
+					terminales.get(terminal).get(fecha)
+				);	
+			}
 		}
-	
-	
+	}
+	private HashMap<Terminal, HashMap<LocalDate, Integer>> recolectarTerminales() {
+		//las llaves son fechas los valores son numeros
+		HashMap<Terminal, HashMap<LocalDate, Integer>> terminales = new HashMap<>();
+		for ( Consulta consulta : this.getRepository().getRegistros() ){
+			Terminal terminal = consulta.getUser();
+			HashMap<LocalDate, Integer> fechas = terminales.getOrDefault(terminal,new HashMap<LocalDate,Integer>());
+			Integer oldValue = fechas.getOrDefault(consulta.getFecha(), 0);
+			Integer newValue = Integer.sum(oldValue, 1);
+			fechas.put(consulta.getFecha(), newValue);
+			terminales.put(terminal, fechas);
+		}
+		return terminales;
+	}
 
-	private JsonObject createResult(String unaTerminal ,LocalDate unaFecha, Long unaCantidad) {
+	private JsonObject createResult(String unaTerminal ,LocalDate unaFecha, Integer integer) {
 		HashMap<String, String> columns = new HashMap<String, String>();
 		columns.put("Terminal", unaTerminal.toString());
 		columns.put("Fecha", unaFecha.toString());
-		columns.put("Cantidad", unaCantidad.toString());
+		columns.put("Cantidad", integer.toString());
 		return super.createResult(columns);
 	}
 
--- src/test/java/poi/ReportesTest/ReporteBusquedaPorTerminalFechaTest.java
+++ src/test/java/poi/ReportesTest/ReporteBusquedaPorTerminalFechaTest.java
@@ -1,16 +1,18 @@
 package poi.ReportesTest;
 
+import java.time.LocalDateTime;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import poi.modelo.usuario.Terminal;
-import poi.reportes.ReporteBusquedasPorTerminal;
+import poi.reportes.ReporteBusquedasPorFechaTerminal;
 import poi.repositorios.RepositorioConsultas;
 import poi.utilidades.Consulta;
 
-public class ReportesPorTerminalTest {
-	ReporteBusquedasPorTerminal reporte = new ReporteBusquedasPorTerminal();
+public class ReporteBusquedaPorTerminalFechaTest {
+	ReporteBusquedasPorFechaTerminal reporte = new ReporteBusquedasPorFechaTerminal();
 	RepositorioConsultas repositorio = RepositorioConsultas.getInstance();
 	Terminal abasto = new Terminal("Abasto");
 	Terminal caballito = new Terminal("Caballito");
@@ -30,6 +32,7 @@ public class ReportesPorTerminalTest {
 	@Test
 	public void cuandoHayConsultasNoDevuelveVacio(){
 		consulta = new Consulta(abasto, "palabra buscada");
+		consulta.setComienzoProceso(LocalDateTime.of(2016, 06, 26, 8, 00));
 		repositorio.agregarRegistro(consulta);
 		reporte.dumpReport();
 		Assert.assertFalse(reporte.getResults().toString().equals("[]"));
@@ -37,28 +40,33 @@ public class ReportesPorTerminalTest {
 	@Test
 	public void cuandoHayConsultasDeMismaTerminal(){
 		consulta = new Consulta(abasto, "palabra buscada 1");
+		consulta.setComienzoProceso(LocalDateTime.of(2016, 06, 26, 8, 00));
 		repositorio.agregarRegistro(consulta);
 		
 		consulta = new Consulta(abasto, "palabra buscada 2");
+		consulta.setComienzoProceso(LocalDateTime.of(2016, 06, 26, 8, 00));
 		repositorio.agregarRegistro(consulta);
-		
+
 		reporte.dumpReport();
-		Assert.assertTrue(reporte.getResults().get(0).toString().equals("{\"Cantidad\":\"2\",\"Terminal\":\"Abasto\"}"));
+		Assert.assertTrue(reporte.getResults().get(0).toString().equals("{\"Fecha\":\"2016-06-26\",\"Cantidad\":\"2\",\"Terminal\":\"Abasto\"}"));
 	}
 	
 	@Test
 	public void cuandoHayConsultasDeDistintasTerminales(){
 		consulta = new Consulta(abasto, "palabra buscada 1");
+		consulta.setComienzoProceso(LocalDateTime.of(2016, 06, 26, 8, 00));
 		repositorio.agregarRegistro(consulta);
 		
 		consulta = new Consulta(abasto, "palabra buscada 2");
+		consulta.setComienzoProceso(LocalDateTime.of(2016, 06, 26, 8, 00));
 		repositorio.agregarRegistro(consulta);
 		
 		consulta = new Consulta(caballito, "palabra buscada 3");
+		consulta.setComienzoProceso(LocalDateTime.of(2016, 06, 1, 15, 22));
 		repositorio.agregarRegistro(consulta);
 		
 		reporte.dumpReport();
-		Assert.assertTrue(reporte.getResults().get(1).toString().equals("{\"Cantidad\":\"2\",\"Terminal\":\"Abasto\"}"));
-		Assert.assertTrue(reporte.getResults().get(0).toString().equals("{\"Cantidad\":\"1\",\"Terminal\":\"Caballito\"}"));
+		Assert.assertTrue(reporte.getResults().get(1).toString().equals("{\"Fecha\":\"2016-06-26\",\"Cantidad\":\"2\",\"Terminal\":\"Abasto\"}"));
+		Assert.assertTrue(reporte.getResults().get(0).toString().equals("{\"Fecha\":\"2016-06-01\",\"Cantidad\":\"1\",\"Terminal\":\"Caballito\"}"));
 	}
 }
