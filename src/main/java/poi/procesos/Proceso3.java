62636fe77b03aac9d83bbfe9c4e3c49ea223e403
NicolasGabrielPerez <nico__1993@hotmail.com>
2016-08-25 18:36:35 -0300

    proceso 3

---
+++ src/main/java/poi/procesos/Proceso3.java
@@ -0,0 +1,33 @@
+package poi.procesos;
+
+import java.util.List;
+import poi.modelo.usuario.Terminal;
+import poi.modelo.usuario.UsuarioPOI;
+import poi.repositorios.RepositorioUsuarios;
+
+
+public class Proceso3 {
+	
+	public Proceso3(){
+	}
+	
+	RepositorioUsuarios terminales = RepositorioUsuarios.getInstance();
+	
+public List<UsuarioPOI> crearActualizacion(List<String> unValor){
+	List<UsuarioPOI> user =  terminales.getTerminals();
+	if (unValor==null) {
+		return user;
+	}
+	else{
+		//TODO: HAACER LO DE COMUNAS 
+		return user;
+	}
+}
+	
+	
+	public void correrProceso(List<String>  unValor, String accion , boolean value) throws Exception{
+		for(UsuarioPOI elemento : crearActualizacion(unValor)) {
+           ((Terminal) elemento).updateAccion(accion, value);
+        }
+	}	
+}
