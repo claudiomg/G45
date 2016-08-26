e3227d6e35769971e460ce7029f244468eaf6119
Claudio <patocmg@yahoo.com.ar>
2016-08-26 12:17:35 -0300

    Agrego proceso actualizacion de locales

---
+++ src/main/java/poi/procesos/ProcesoActualizacionLocales.java
@@ -0,0 +1,95 @@
+package poi.procesos;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import poi.modelo.puntoDeInteres.LocalComercial;
+import poi.modelo.puntoDeInteres.POI;
+import poi.repositorios.RepositorioAbstractoPOI;
+import poi.repositorios.RepositorioPOI;
+
+public class ProcesoActualizacionLocales {
+
+	public void correrProceso(){
+		List <String> lista = this.cargaLista();
+		String nombre = lista.get(0);		
+		List <String> palabrasClave = new ArrayList<String>();
+		for (int i = 1; i==lista.size() - 1; i++ ){
+			palabrasClave.add(lista.get(i));
+		}
+		if(existePOI(nombre)){
+			cambiarEtiquetas(nombre, palabrasClave);
+		} else {
+			agregarPOI(nombre, palabrasClave);
+		}	
+	}
+	
+	public void cambiarEtiquetas(String nombre, List<String> etiquetas){
+		RepositorioAbstractoPOI repositorioLocal = RepositorioPOI.getInstance();		
+		repositorioLocal.getRegistros().stream()
+			.filter(local -> ((local.getClass()==LocalComercial.class) && (nombre == local.getNombre())))
+			.findFirst().get().setPalabrasClave(etiquetas);
+	}
+
+	public void agregarPOI(String nombre, List<String> etiquetas){
+		RepositorioAbstractoPOI repositorioLocal = RepositorioPOI.getInstance();		
+		POI nuevoLocal = new LocalComercial(nombre, null, null, null);
+		nuevoLocal.setPalabrasClave(etiquetas);
+		repositorioLocal.agregarRegistro(nuevoLocal);
+	}
+
+	public boolean existePOI(String nombre){
+		RepositorioAbstractoPOI repositorioLocal = RepositorioPOI.getInstance();		
+		return repositorioLocal.getRegistros().stream()
+				.anyMatch(local -> ((local.getClass()==LocalComercial.class) && (nombre == local.getNombre())));
+	}
+
+	public List<String> cargaLista(){
+		List <String> lista = new ArrayList<String>();
+		try{
+		   FileReader fr = new FileReader("c:/actualizacion.txt");
+		   int caract = fr.read();
+		   String nombre = "";
+		   String etiqueta = "";
+		   boolean nombreOK = false;
+		   boolean etiquetaOK = false;
+		   while(caract != -1){
+			   while(nombreOK == false){
+				   char caracter = (char)caract;			   
+				   String prueba = Character.toString(caracter);			   
+				   nombre += prueba;
+				   caract = fr.read();
+				   if (caract == 59){
+					   nombreOK = true;
+					   lista.add(nombre);
+					   caract = fr.read();
+				   }
+			   }
+			   while((caract != -1) && (etiquetaOK == false) && (caract != 32)){
+				   char caracter = (char)caract;			   
+				   String prueba = Character.toString(caracter);			   
+				   etiqueta += prueba;
+				   caract = fr.read();
+				   if ((caract == 32) || (caract == -1)){
+					   lista.add(etiqueta);
+					   etiqueta = "";
+					   etiquetaOK = true;
+				   }
+			   }
+			   etiquetaOK = false;
+			   caract = fr.read();
+		   }
+		   
+		   fr.close();
+		}
+		catch (Exception e){
+		   System.out.println("Error de lectura del archivo");
+		}				
+		return lista;
+	}
+}
+
