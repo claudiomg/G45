2a4b8483a4c36a4c30969b6702f347b2dbbd637e
Kevin N <kevin.nielsen93@gmail.com>
2016-10-25 23:57:09 -0300

    Algunos arreglos en el test 2

--- src/test/java/poi/entrega6/PoiNoEncontradoTest.java
+++ src/test/java/poi/entrega6/PoiNoEncontradoTest.java
@@ -1,5 +1,7 @@
 package poi.entrega6;
 
+import java.time.LocalDateTime;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,70 +13,69 @@ import poi.utilidades.Direccion;
 import poi.utilidades.ExcepcionSinAtencion;
 import poi.utilidades.Posicion;
 
-public class PoiNoEncontradoTest implements WithGlobalEntityManager{
+public class PoiNoEncontradoTest implements WithGlobalEntityManager {
 
 	/*
-	Posicion posicion = new Posicion(40.417, 3.703);
-	Direccion direccion = new Direccion();
-	LocalComercial kiosco = new LocalComercial("kiosco1",posicion,direccion,RadioCercania.Kiosco);
-	
-	@Before
-	public void setUp(){
-		
+	 * Posicion posicion = new Posicion(40.417, 3.703); Direccion direccion =
+	 * new Direccion(); LocalComercial kiosco = new
+	 * LocalComercial("kiosco1",posicion,direccion,RadioCercania.Kiosco);
+	 * 
+	 * @Before public void setUp(){
+	 * 
+	 * direccion.setBarrio("Almagro"); direccion.setLocalidad("Chivilcoy");
+	 * direccion.setCalle("Guemes"); direccion.setCodigoPostal("2222");
+	 * 
+	 * kiosco.setDireccion(direccion); }
+	 */
+	@Test
+	public void poiNoEncontrado() {
+
+		Posicion posicion = new Posicion(40.417, 3.703);
+		Direccion direccion = new Direccion();
+		LocalComercial kiosco = new LocalComercial("kiosco1", posicion,
+				direccion, RadioCercania.Kiosco);
+		ExcepcionSinAtencion feriados = new ExcepcionSinAtencion();
+
 		direccion.setBarrio("Almagro");
 		direccion.setLocalidad("Chivilcoy");
 		direccion.setCalle("Guemes");
 		direccion.setCodigoPostal("2222");
-		
-		kiosco.setDireccion(direccion);
-	}
-	*/
-	@Test
-	public void poiNoEncontrado(){
-		
-		Posicion posicion = new Posicion(40.417, 3.703);
-		Direccion direccion = new Direccion();
-		LocalComercial kiosco = new LocalComercial("kiosco1",posicion,direccion,RadioCercania.Kiosco);
-		ExcepcionSinAtencion feriados = new ExcepcionSinAtencion();
 
-			
-			direccion.setBarrio("Almagro");
-			direccion.setLocalidad("Chivilcoy");
-			direccion.setCalle("Guemes");
-			direccion.setCodigoPostal("2222");
-			
-			kiosco.setDireccion(direccion);
-			kiosco.setFeriados(feriados);
-			
-		//Crea y persiste POI
+		feriados.agregarFeriados(LocalDateTime.of(0, 7, 9, 0, 0));
+		feriados.agregarFeriados(LocalDateTime.of(0, 5, 1, 0, 0));
+
+		kiosco.setDireccion(direccion);
+		kiosco.setFeriados(feriados);
+
+		// Crea y persiste POI
 		entityManager().getTransaction().begin();
+		entityManager().persist(feriados);
 		entityManager().persist(kiosco);
 		entityManager().getTransaction().commit();
-		
-		//Recupera POI
+
+		// Recupera POI
 		String nombreABuscar = "kiosco1";
 		Long idRecuperado;
-		
-		idRecuperado = entityManager() 
-		.createQuery("from LocalComercial l where l.nombre like :nombre", LocalComercial.class) 
-		.setParameter("nombre", "%" + nombreABuscar + "%")
-		.getSingleResult()
-		.getId();
-		
-		//Elimina POI
+
+		idRecuperado = entityManager()
+				.createQuery(
+						"from LocalComercial l where l.nombre like :nombre",
+						LocalComercial.class)
+				.setParameter("nombre", "%" + nombreABuscar + "%")
+				.getSingleResult().getId();
+
+		// Elimina POI
 		entityManager().getTransaction().begin();
 		entityManager().remove(kiosco);
 		entityManager().getTransaction().commit();
-		
-		//Busca POI eliminado
-		
-		
-		Assert.assertTrue( entityManager().createQuery("from LocalComercial l where l.PoiId = :id", LocalComercial.class).setParameter("id", idRecuperado).getSingleResult() == null);
-		
-		
-		
-		
-		
+
+		// Busca POI eliminado
+
+		Assert.assertTrue(entityManager()
+				.createQuery("from LocalComercial l where l.PoiId = :id",
+						LocalComercial.class).setParameter("id", idRecuperado)
+				.getResultList().isEmpty());
+
 	}
-	
+
 }
